1c220d660fa5f4c5681e0d54451cb424fe55af97
PetitMais <hana.belin@gmail.com>
2024-06-05 11:53:51 +0200

    update

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const registerServiceWorker = async () => {
-    if("serviceWorker" in navigator) {
-        try {
-            const registration = await navigator.serviceWorker.register("/sw.js", {
-                scope: "/",
-            });
-            if (registration.installing) {
-                console.log("Installation du service worker en cours");   
-            } else if (registration.waiting) {
-                console.log("Service worker installé");
-            } else if (registration.active) {
-                console.log("Service worker actif");
-            }
-        } catch (error) {
-            console.log(`l'enregistrement a échoué : ${error}`);
-        }
-    }
-};
-registerServiceWorker();
+// const registerServiceWorker = async () => {
+//     if("serviceWorker" in navigator) {
+//         try {
+//             const registration = await navigator.serviceWorker.register("/sw.js", {
+//                 scope: "/",
+//             });
+//             if (registration.installing) {
+//                 console.log("Installation du service worker en cours");   
+//             } else if (registration.waiting) {
+//                 console.log("Service worker installé");
+//             } else if (registration.active) {
+//                 console.log("Service worker actif");
+//             }
+//         } catch (error) {
+//             console.log(`l'enregistrement a échoué : ${error}`);
+//         }
+//     }
+// };
+// registerServiceWorker();
