f30a131aa013bdb144568a0e9085fe824b842f9c
PetitMais <hana.belin@gmail.com>
2024-06-15 15:10:15 +0200

    update

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
 self.addEventListener('fetch', (event) => {
-  console.log(`Fetching : ${event.request.url}`);
+  console.log(`Fetching : ${event.request.url}, Mode : ${event.request.mode}`);
+  
+  if (event.request.mode === 'navigate') {
+    event.respondWidh(
+      (async () => {
+        return new Response('Bonjour les gens')
+      })()
+    );
+  }
 });
